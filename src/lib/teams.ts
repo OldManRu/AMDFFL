dfa08cbb4d975ed73748ca6d4722252f6552e244
OldManRu <warmachine519@gmail.com>
2026-08-13 18:25:43 -0500

    Isolate team catalog for franchise page

---
+++ src/lib/teams.ts
@@ -0,0 +1,7 @@
+import teamsA from '../../data/teams/01-07.json';
+import teamsB from '../../data/teams/08-10.json';
+import teamsC from '../../data/teams/11-12.json';
+import teamsD from '../../data/teams/13.json';
+import teamsE from '../../data/teams/14.json';
+
+export const teams = [...teamsA, ...teamsB, ...teamsC, ...teamsD, ...teamsE];
